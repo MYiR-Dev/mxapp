46d8d9411c0bcfe35722db3fa6be93e956117ab0
dy <dy.dong@myir.cn>
2026-05-27 15:57:06 +0800

    FIX: Optimize the repository content

--- main.cpp
+++ main.cpp
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 
     app.setOrganizationName("MYiR_Electronics");
     app.setApplicationName("MEasy_HMI");
-    app.setApplicationVersion("V2.4");
+    app.setApplicationVersion("V2.4.1");
 
     QQmlApplicationEngine engine;
 
